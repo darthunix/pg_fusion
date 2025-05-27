0b77b8c1158236301c01fcf10d7b5bd5c887412f
Denis Smirnov <darthunix@gmail.com>
2025-05-27 11:07:13 +0700

    test: extend ipc test coverage

--- core/src/ipc.rs
+++ core/src/ipc.rs
@@ -81,3 +81,66 @@ impl Future for Socket<'_> {
         Poll::Pending
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::buffer::LockFreeBuffer;
+    use std::cell::UnsafeCell;
+    use std::time::Duration;
+    use tokio::task;
+    use tokio::time::timeout;
+
+    struct Memory {
+        bytes: UnsafeCell<[u8; 8 + 13]>,
+        flags: UnsafeCell<[AtomicBool; 1]>,
+    }
+    impl Memory {
+        const fn new() -> Self {
+            Self {
+                flags: UnsafeCell::new([AtomicBool::new(false); 1]),
+                bytes: UnsafeCell::new([0; 8 + 13]),
+            }
+        }
+    }
+    unsafe impl Sync for Memory {}
+
+    #[tokio::test]
+    async fn test_signal_before_poll() -> Result<()> {
+        let flags = [AtomicBool::new(true)];
+        let state = Arc::new(SharedState::new(&flags));
+
+        let mut bytes = vec![0u8; 8 + 13];
+        let buffer = LockFreeBuffer::new(&mut bytes);
+        let socket = Socket::new(0, Arc::clone(&state), buffer);
+
+        // As the flag is already set, .await should complete immediately.
+        timeout(Duration::from_secs(1), socket).await??;
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_signal_after_poll() -> Result<()> {
+        static BUFFER: Memory = Memory::new();
+
+        let state = Arc::new(SharedState::new(unsafe { &*BUFFER.flags.get() }));
+        let buffer = LockFreeBuffer::new(unsafe { &mut *BUFFER.bytes.get() });
+        let socket = Socket::new(0, Arc::clone(&state), buffer);
+
+        let handle = task::spawn(socket);
+
+        // Wait for socket to register its waker.
+        tokio::time::sleep(Duration::from_millis(100)).await;
+
+        // Set the flag.
+        unsafe {
+            (&mut *BUFFER.flags.get())[0].store(true, Ordering::Release);
+        }
+        // Manually wake the waker.
+        state.wakers[0].wake();
+
+        // Future should succeed.
+        timeout(Duration::from_secs(1), handle).await???;
+        Ok(())
+    }
+}
